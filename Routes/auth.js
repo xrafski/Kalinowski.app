866e4f3ae2a05ff33c314d75a9019315291d3ed0
Skillez <page+github@skillez.eu>
2021-11-01 01:44:36 +0100

    3

--- Routes/auth.js
+++ Routes/auth.js
@@ -6,9 +6,7 @@ const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const { jtwSecret } = require('../Settings/secret/info.json'); // Secret file
 
-
 // router domain/
-
 
 router.get('/', async (req, res) => {
     res.status(200).send('Home route');
@@ -17,11 +15,11 @@ router.get('/', async (req, res) => {
 router.post('/register', async (req, res) => {
     // Validate the data
     const { error } = registerValidation(req.body);
-    if (error) return res.status(400).send(error.details[0].message);
+    if (error) return res.status(400).send({ error: error.details[0].message });
 
     // Check if user is already registered
     const userExist = await userModel.findOne({ username: req.body.username });
-    if (userExist) return res.status(400).send('This user is already registered');
+    if (userExist) return res.status(400).send({ error: 'This user is already registered' });
 
     // Hash the password
     const salt = bcrypt.genSaltSync(10);
@@ -35,30 +33,29 @@ router.post('/register', async (req, res) => {
 
     try {
         const savedUser = await user.save();
-        res.status(200).send(`User '${savedUser.username}' registered successfully.`);
+        res.status(200).send({ message: `User '${savedUser.username}' registered successfully.` });
     } catch (err) {
-        res.status(400).send(err);
+        res.status(400).send({ error: err.message });
     }
 });
 
 router.post('/login', async (req, res) => {
     // Validate the data
     const { error } = loginValidation(req.body);
-    if (error) return res.status(400).send(error.details[0].message);
+    if (error) return res.status(400).send({ error: error.details[0].message });
 
     // Check if user exists
     const dbUser = await userModel.findOne({ username: req.body.username, activated: true });
-    if (!dbUser) return res.status(400).send('This user is not registered or activated.');
+    if (!dbUser) return res.status(400).send({ error: 'This user is not registered or activated.' });
 
     // Check if password is correct
     const validPassword = bcrypt.compareSync(req.body.password, dbUser.password);
-    if (!validPassword) return res.status(400).send('Invalid password.');
+    if (!validPassword) return res.status(400).json({ error: 'Invalid password.' });
 
     // Create and assign json web token.
     const token = jwt.sign({ _id: dbUser._id }, jtwSecret);
-    res.header('kali-token', token).send(token);
-
-    // res.status(200).send('Logged in');
+    // res.setHeader('Content-Type', 'application/json');
+    res.status(200).header('kali-token', token).send({ message: 'Logged in successfully', token });
 });
 
 module.exports = router;

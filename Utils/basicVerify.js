01ce39e6496f1dc654fe335d37cf79470d12e6e9
Skillez <page+github@skillez.eu>
2021-11-04 12:21:20 +0100

    Added logs for auth middleware.

--- Utils/basicVerify.js
+++ Utils/basicVerify.js
@@ -3,30 +3,42 @@ const { loginValidation } = require('./validation');
 const bcrypt = require('bcryptjs');
 
 module.exports = async function (req, res, next) {
-
     // Check for basic auth header
     if (!req.headers.authorization || req.headers.authorization.indexOf('Basic ') === -1) {
+        console.warn(`[API] Unauthorized user with missing authorization header tried to use '${req.originalUrl}' route with '${req.method}' method.`);
         return res.status(401).json({ message: 'Missing Authorization Header' });
     }
 
-    // verify auth credentials
+    // Verify auth credentials
     const base64Credentials = req.headers.authorization.split(' ')[1];
     const credentials = Buffer.from(base64Credentials, 'base64').toString('ascii');
     const [username, password] = credentials.split(':');
 
-    // validate the data
+    // Validate the data
     const { error } = loginValidation({ username, password });
-    if (error) return res.status(400).send({ error: error.details[0].message });
+    if (error) {
+        console.warn(`[API] Unauthorized user '${username}' with invalid authorization header tried to use '${req.originalUrl}' route with '${req.method}' method.`);
+        return res.status(400).send({ message: error.details[0].message });
+    }
 
     // Check if user exists
     const dbUser = await userModel.findOne({ username, activated: true });
-    if (!dbUser) return res.status(401).send({ error: 'This user is not registered or activated.' });
+    if (!dbUser) {
+        console.warn(`[API] Unauthorized user '${username}' tried to use '${req.originalUrl}' route with '${req.method}' method.`);
+        return res.status(401).send({ message: 'This user is not registered or activated.' });
+    }
+
+    // Assign userDB to req.user
+    req.user = dbUser._doc.username;
 
     // Check if password is correct
     const validPassword = bcrypt.compareSync(password, dbUser.password);
-    if (!validPassword) return res.status(401).json({ error: 'Invalid password.' });
+    if (!validPassword) {
+        console.warn(`[API] Unauthorized user '${req.user}' with invalid password tried to use '${req.originalUrl}' route with '${req.method}' method.`);
+        return res.status(401).json({ message: 'Invalid password.' });
+    }
 
-    // Assign userDB doc to req.user
-    req.user = dbUser._doc;
+    // Console.info that event out.
+    console.info(`[API] '${req.user}' used '${req.originalUrl}' route with '${req.method}' method.`);
     next();
 };

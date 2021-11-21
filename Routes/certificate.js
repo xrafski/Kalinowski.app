95f58de198ba4c11ba915811ec09fe11b73b03a2
Skillez <page+github@kalinowski.app>
2021-11-21 12:38:20 +0100

    Added route for all certificates

--- Routes/certificate.js
+++ Routes/certificate.js
@@ -3,6 +3,17 @@ const { mongoCertificate } = require('../Models/Certificates');
 const router = express.Router();
 const basicVerify = require('../Utils/basicVerify');
 
+// List all certificates.
+router.get('/', basicVerify, async (req, res) => {
+    try {
+        const dbGuild = await mongoCertificate.find({});
+        res.status(200).send(dbGuild);
+    } catch (error) {
+        res.status(500).send({ message: error.toString() });
+    }
+});
+
+// List a specific certificate.
 router.get('/:guild', basicVerify, async (req, res) => {
     try {
         const query = { $or: [{ club: { $regex: req.params.guild, $options: 'i' } }, { 'discord.id': { $regex: req.params.guild, $options: 'i' } }] };
